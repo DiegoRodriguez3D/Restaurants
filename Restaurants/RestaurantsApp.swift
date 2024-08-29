de95826fe81f28967c751b0948acca3e88224d90
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2024-08-29 12:54:32 +0200

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Diego Rodriguez on 29/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
